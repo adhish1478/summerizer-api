973405eeea678dc0ae1d32e215c46dd8baea7f36
adhish1478 <adhish.aravind7@gmail.com>
2025-04-23 19:51:30 +0530

    discarded nltk and used regex

--- summerizer_app/views.py
+++ summerizer_app/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import SummarizeTextSerializer
+
 
 # Create your views here.
 
 class SummarizeText(APIView):
     def post(self, request):
-        text= request.data.get('text')
-        return Response({'message' : 'Text received', 'text': text})
+        serializer= SummarizeTextSerializer(data= request.data)
+
+        if serializer.is_valid():
+            text= serializer.validated_data['text']
+            import re
+            text= re.sub(r'\s+', ' ', text).strip() # remove special characters
+            text= ' '.join(text.split()) # remove spcaes
+            #Summary logic
+            sentences = re.split(r'(?<=[.!?]) +', text)
+            summary= ' '.join(sentences[:3]) # taking first 3 sentences as summary
+            return Response({'summary': summary})
+        else:
+            return Response(serializer.errors, status=400)
